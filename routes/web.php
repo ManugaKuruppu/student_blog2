ea321d8d5199022901868089492d4cb22a85021a
ManugaKuruppu <103434569+ManugaKuruppu@users.noreply.github.com>
2024-03-30 02:57:21 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Http\Controllers\HomeController::class)->name('home');
+
 
 Route::middleware([
     'auth:sanctum',
